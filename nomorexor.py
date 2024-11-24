541b514d31e3c74eba005e666c6b5093e487f235
digitalsleuth <corey@digitalsleuth.ca>
2024-11-24 04:21:24 +0000

    Update nomorexor

--- nomorexor.py
+++ nomorexor.py
@@ -1,34 +1,35 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # NoMoreXOR.py was created by Glenn P. Edwards Jr.
 #	http://hiddenillusion.blogspot.com
 #		@hiddenillusion
-# Version 0.0.2
-# Date: 12-11-2012
+# Version 1.0.0
+# Date: 06-09-2023
 # To-Do:
 #	- add YARA scanning to try and detect ROR/ROL
 #	- add ability to ROL/ROR then XOR
 #	- process other size xor? (i.e. single byte)
 #	- add ability to skip certain bytes (i.e. \x00)
+
 import os
 import re
 import sys
 import argparse
 import binascii
 import collections
-from itertools import izip, cycle
+
 try:
     import yara
 except ImportError:
-    print "[!] Yara module not installed"
+    print("[!] Yara module not installed")
     sys.exit()	
 
 # Formatting jargon
 def header(msg):
-    return msg + "\n" + ("=" * 45)
+    return f"{msg}\n{('=' * 45)}"
 
 def notes():
-    return "\tOccurences | Character(s)" + "\n" + ("-" * 45)
+    return f"\tOccurences | Character(s)\n{('-' * 45)}"
 
 # What did you give me to do?
 parser = argparse.ArgumentParser(description='Tool to help guess a files 256 byte XOR key by using frequency analysis.')
@@ -47,7 +48,7 @@ filename = args['Path']
 # output file is required for: analyze,convert,xor
 if not args['out']:
     if args['analyze'] or args['convert'] or args['xor']:
-        print "[!] An output file is required for this task"
+        print("[!] An output file is required for this task")
         sys.exit()
 else: 
     new_file = args['out']
@@ -60,43 +61,43 @@ if args['xor']:
 if not args['yararules']:
     rules = '/path/to/rules.yara'
 else:
-   rules = args['yararules']
+    rules = args['yararules']
 
 # Placeholders
 keys = []
 
 # Configure YARA rules
 def yarascan(filename):
-    print "[+] Scanning with Yara"
-    print "[-] Using rules" + ('.'*28 + ': ') + rules
+    print("[+] Scanning with Yara")
+    print(f"[-] Using rules{('.'*28 + ': ')}{rules}")
     if not os.path.exists(rules):
-            print "[!] Correct path to YARA rules?"
+            print("[!] Correct path to YARA rules?")
             sys.exit()
     try:
         r = yara.compile(rules)
         ymatch = r.match(filename)
         if len(ymatch):
-            print "[+] YARA hit(s):",ymatch
-            print "[+] Possible XOR key:",key
-            print "[+] Un-XOR'ed file should be examined:",unxored
-            print "[+] Stopping processing"
+            print(f"[+] YARA hit(s): {ymatch}")
+            print(f"[+] Possible XOR key: {key}")
+            print(f"[+] Un-XOR'ed file should be examined: {unxored}")
+            print("[+] Stopping processing")
             sys.exit()
-    except Exception, msg:
-        print "[!] YARA compile error: %s" % msg
+    except Exception as msg:
+        print(f"[!] YARA compile error: {msg}")
         sys.exit()
 
 # HEX it up?
 def hexMeBro(filename):
-    print "[+] HEXing" + ('.'*32 + ': ') + filename
+    print(f"[+] HEXing {('.'*32 + ': ')}{filename}")
     with open(filename, 'rb') as f:
         content = f.read()
-        print "[+] Saving as" + ('.'*29 + ': ') + new_file
-        with open(new_file, 'w') as nf:
+        print(f"[+] Saving as {('.'*29 + ': ')}{new_file}")
+        with open(new_file, 'wb') as nf:
             nf.write(binascii.hexlify(content))
 
 # Attempt to guess the XOR key with frequency analysis
 def guess(filename):
-    print "[+] Attempting to guess the XOR key of....:",filename
+    print(f"[+] Attempting to guess the XOR key of ....: {filename}")
     with open(filename, 'r') as f:
         content = f.read()
 
@@ -137,18 +138,18 @@ def guess(filename):
     Return the results and make it look purdy...
     """
     results = []
-    results.append("[+] Size of content.......................: %d" % length)
-    results.append("[+] Total pages (1024k)...................: %d" % (length / int(1024)))
-    results.append("[+] Total contiguous 512 chunks...........: %d" % (length / int(512)))
+    results.append(f"[+] Size of content.......................: {length}")
+    results.append(f"[+] Total pages (1024k)...................: {length / int(1024)}")
+    results.append(f"[+] Total contiguous 512 chunks...........: {length / int(512)}")
     results.append(header("[+] Top (5) overall chars"))
     results.append(notes())
     results.append(charCounter())
     results.append("")
-    results.append("[+] Total number of unique 512 chunks.....: %s" % len(char_mucho))
+    results.append(f"[+] Total number of unique 512 chunks.....: {len(char_mucho)}")
     results.append(header("[+] Top (5) 512 char sequences after cleanup"))
     results.append(notes())
     results.append(five_twelver())
-    print '\n'.join(results)
+    print('\n'.join(results))
 
 # XOR that baby
 def hex2str(h):
@@ -170,7 +171,7 @@ def xor(content, key):
     I know there're other methods to do the same thing
     and they're probably faster too, but this works for now
     """
-    print "[+] XOR'ing" + ('.'*32 + ': ') + filename
+    print(f"[+] XOR'ing {('.'*32 + ': ')}{filename}")
     dlen = len(content)
     klen = len(key)
     decoded = ''
@@ -189,7 +190,7 @@ if args['convert']:
 elif args['guess']:
     guess(filename) 
 elif args['xor']:
-    print "[+] key used : %s" % key
+    print(f"[+] key used : {key}")
     with open(filename, 'r') as f:
         content = f.read()
     """
@@ -197,9 +198,9 @@ elif args['xor']:
     """
     with open(new_file, 'wb') as nf:
         nf.write(xor(content, hex2str(key)))
-    print "[+] Saving as: %s" % new_file
+    print(f"[+] Saving as: {new_file}")
 elif args['analyze']:
-    print "[+] Attempting auto analysis" 
+    print("[+] Attempting auto analysis")
     #1 - open file in hexMeBro(), hex it and saves as new_file
     hexMeBro(filename)
     #2 - send new_file 'hex_file' to guess()
@@ -207,15 +208,15 @@ elif args['analyze']:
     #3 - for each possible key found, try that key against the original file 'filename'
     i = 0
     for key,occur in keys:
-        unxored = filename + '.' + str(i) + '.' + 'unxored'
+        unxored = f"{filename}.{str(i)}.unxored"
         #4 - xor original file 'filename' with key
-        print "[+] Trying XOR key :",key
+        print(f"[+] Trying XOR key: {key}")
         #4 - xor original file 'filename' with key
         with open(filename, 'r') as f:
             content = f.read()
         with open(unxored, 'wb') as nf:
             nf.write(xor(content, hex2str(key)))
-        print "[+] Saving as" + ('.'*30 + ': ') + unxored
+        print(f"[+] Saving as {('.'*30 + ': ')}{unxored}")
         #5 - yarascan newly saved file.(xored number).unxored
         yarascan(unxored)
         i += 1
